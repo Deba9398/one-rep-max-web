8a98c19dae121843ac338b3806f81c9a74e0770b
Derek Baumgartner <Derek.Baumgartner@gmail.com>
2023-11-10 23:16:52 -0700

    Responsive Design

--- components/OneRepMaxCalc.tsx
+++ components/OneRepMaxCalc.tsx
@@ -22,90 +22,92 @@ export function OneRepMaxCalc() {
   const repMaxValues = calculateRepMaxValues(weightLifted, repsPerformed);
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between'>
-      <div className='flex items-center justify-between'>
-        <Box maw={600} mx='auto'>
-          <div className='flex items-end pb-4 text-center'>
-            <Button
-              size='xl'
-              disabled={weightLifted <= 0}
-              onClick={() => weightLiftedRef.current?.decrement()}
-              variant='default'
-            >
-              -
-            </Button>
-            <NumberInput
-              className='px-1 text-center'
-              size='xl'
-              allowNegative={false}
-              inputMode='decimal'
-              suffix=' lbs'
-              min={0}
-              max={1000}
-              step={5}
-              handlersRef={weightLiftedRef}
-              onChange={(val) => setWeightLifted(parseFloat(val.toString()))}
-              value={weightLifted}
-              label='Weight'
-              hideControls
-            />
-            <Button
-              size='xl'
-              disabled={weightLifted >= 1000}
-              onClick={() => weightLiftedRef.current?.increment()}
-              variant='default'
-            >
-              +
-            </Button>
+    <main className='flex flex-col items-center'>
+      <Box maw={600} mx='auto'>
+        <div className='flex items-end pb-4 text-center'>
+          <Button
+            size='xl'
+            disabled={weightLifted <= 0}
+            onClick={() => weightLiftedRef.current?.decrement()}
+            variant='default'
+          >
+            -
+          </Button>
+          <NumberInput
+            className='px-1 text-center'
+            size='xl'
+            allowNegative={false}
+            inputMode='decimal'
+            suffix=' lbs'
+            min={0}
+            max={1000}
+            step={5}
+            handlersRef={weightLiftedRef}
+            onChange={(val) => setWeightLifted(parseFloat(val.toString()))}
+            value={weightLifted}
+            label='Weight'
+            hideControls
+          />
+          <Button
+            size='xl'
+            disabled={weightLifted >= 1000}
+            onClick={() => weightLiftedRef.current?.increment()}
+            variant='default'
+          >
+            +
+          </Button>
+        </div>
+
+        <div className='flex items-end pb-8 text-center'>
+          <Button
+            size='xl'
+            disabled={repsPerformed <= 1}
+            onClick={() => repsPerformedRef.current?.decrement()}
+            variant='default'
+          >
+            -
+          </Button>
+          <NumberInput
+            className='px-1 text-center'
+            size='xl'
+            inputMode='decimal'
+            suffix=' reps'
+            clampBehavior='strict'
+            allowNegative={false}
+            min={0}
+            max={15}
+            handlersRef={repsPerformedRef}
+            onChange={(val) => setRepsPerformed(parseFloat(val.toString()))}
+            value={repsPerformed}
+            label='Reps'
+            hideControls
+          />
+          <Button
+            size='xl'
+            disabled={repsPerformed >= 15}
+            onClick={() => repsPerformedRef.current?.increment()}
+            variant='default'
+          >
+            +
+          </Button>
+        </div>
+      </Box>
+
+      <div className='container mx-auto'>
+        <div className='flex flex-col items-center justify-center xl:flex-row xl:items-start -mx-2 lg:-mx-0'>
+          <div className='px-2 xl:px-4 w-full xl:w-1/2 max-w-[600px]'>
+            <Title order={3} className='pb-4 pt-4 text-center'>
+              Rep Max
+            </Title>
+            <RepMaxTable repMaxValues={repMaxValues} />
           </div>
-
-          <div className='flex items-end pb-8 text-center'>
-            <Button
-              size='xl'
-              disabled={repsPerformed <= 1}
-              onClick={() => repsPerformedRef.current?.decrement()}
-              variant='default'
-            >
-              -
-            </Button>
-            <NumberInput
-              className='px-1 text-center'
-              size='xl'
-              inputMode='decimal'
-              suffix=' reps'
-              clampBehavior='strict'
-              allowNegative={false}
-              min={0}
-              max={15}
-              handlersRef={repsPerformedRef}
-              onChange={(val) => setRepsPerformed(parseFloat(val.toString()))}
-              value={repsPerformed}
-              label='Reps'
-              hideControls
-            />
-            <Button
-              size='xl'
-              disabled={repsPerformed >= 15}
-              onClick={() => repsPerformedRef.current?.increment()}
-              variant='default'
-            >
-              +
-            </Button>
+          <div className='px-2 xl:px-4 w-full xl:w-1/2 max-w-[600px]'>
+            <Title order={3} className='pb-4 pt-4 text-center'>
+              Percentages of 1RM
+            </Title>
+            <PercentagesTable repMaxValues={repMaxValues} />
           </div>
-
-          <Title order={3} className='pb-4 pt-4 text-center'>
-            Rep Max
-          </Title>
-          <RepMaxTable repMaxValues={repMaxValues} />
-
-          <Space h='lg' />
-          <Title order={3} className='pb-4 pt-4 text-center'>
-            Percentages of 1RM
-          </Title>
-          <PercentagesTable repMaxValues={repMaxValues} />
-
-          <Space h='lg' />
-        </Box>
+        </div>
       </div>
     </main>
   );
